7629fcd270cb4e65a68790915afca2834c8395fb
Fabien Thiombiano <thiombianofabien@gmail.com>
2023-11-15 02:23:34 +0000

    Property binding, interpolation and event binding

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent {
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server was created!';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
+}
